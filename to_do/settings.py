453a4d4dd2599fb8efd3eafc18c0775d9826b7b5
vitortamberlini <vitor.tamberlini@outlook.com>
2025-09-30 22:35:11 +0000

    👌

--- to_do/settings.py
+++ to_do/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en_US'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
